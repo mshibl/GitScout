82cbd7fc4a5449dca49e8c0f8b441152edcfcf02
mshibl <mohamed.shibl@hotmail.com>
2016-09-23 04:23:44 -0700

    attempting to fix heroku deployment

--- client/components/App.js
+++ client/components/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import MainSearch from './MainSearch'
 import User from './User';
-import { Router, Route, Link, browserHistory } from 'react-router'
+import { Router, Route, browserHistory } from 'react-router'
 
 export default class App extends React.Component {
-	// const WrapperComponent = withRouter(MyComponent, { withRef: true })
 	render(){
 		return(
 			<Router history={browserHistory}>
