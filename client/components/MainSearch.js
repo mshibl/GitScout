cd11c9f5cadee7c03f826511d700b57a44350990
mshibl <mohamed.shibl@hotmail.com>
2016-09-23 04:25:52 -0700

    installs cross-env

--- client/components/MainSearch.js
+++ client/components/MainSearch.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {observer} from "mobx-react"
-import {Link} from 'react-router'
 
 import store from '../utils/store';
 import Input from './input.js'
@@ -11,12 +10,6 @@ export default observer(class MainSearch extends React.Component {
 			<div className="input-list">
 				<p className="main-guide">Who is the Github user you wish to find?</p>
 				<Input verified={store.mainUser.verified == "verified"} />
-				{store.mainUser.verified == "verified" ? 
-					<button type="button">
-						<Link className="results-button" to={'/user/'+store.mainUser.username}>Show Results</Link>
-					</button> 
-					: null
-				}
 			</div>
 		)
 	}
