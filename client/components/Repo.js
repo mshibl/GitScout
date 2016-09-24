7810af417d98174738c404770fb2fbfa6fb58153
mshibl <mohamed.shibl@hotmail.com>
2016-09-24 01:34:52 -0700

    fixing typo in url

--- client/components/Repo.js
+++ client/components/Repo.js
@@ -6,7 +6,7 @@ export default class Repo extends React.Component {
 		return(
 		     <div className="col-md-3 col-xs-12">
 		     	<div className="repo col-md-12">
-			     	<div className="name"> <a href={repo.url}> {repo.name} </a> </div>
+			     	<div className="name"> <a href={repo.html_url}> {repo.name} </a> </div>
 			     	<div className="stars"> <i className="fa fa-star" aria-hidden="true" /> {repo.stargazers_count} </div>
 			     	<div className="language"> <i className="fa fa-pencil" aria-hidden="true" /> {repo.language ? repo.language : "N/A"} </div>
 		     	</div>
