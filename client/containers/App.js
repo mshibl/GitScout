8e0cba4428102bcf4d3f1a3d70af2f34684b971a
mshibl <mohamed.shibl@hotmail.com>
2016-09-23 18:08:16 -0700

    removes github_token if undefined

--- client/containers/App.js
+++ client/containers/App.js
@@ -4,6 +4,10 @@ import UserProfile from './UserProfile';
 import { Router, Route, browserHistory } from 'react-router'
 
 export default class App extends React.Component {
+	componentWillMount() {
+	 	if(sessionStorage.getItam('github_token') == undefined) sessionStorage.removeItem('github_token')
+	}
+
 	render(){
 		return(
 			<div>
